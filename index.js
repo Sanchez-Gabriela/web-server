3b3b833e5d10460be8cc53e5cfb5ddfd997732eb
Gabriela Sanchez <gaby.peta@gmail.com>
2020-05-08 16:08:10 +0200

    ADD LAST COMMIT

---
+++ index.js
@@ -0,0 +1,11 @@
+let http = require('http');
+
+let nStatic = require('node-static');
+
+let fileServer = new nStatic.Server('./public');
+
+http
+  .createServer(function (req, res) {
+    fileServer.serve(req, res);
+  })
+  .listen(8080);
